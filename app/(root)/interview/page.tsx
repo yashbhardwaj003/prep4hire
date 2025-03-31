ac447d8ef458622d79910b022b64100484a7b17a
yashbhardwaj003 <yxrock101fsd@gmail.com>
2025-03-31 12:03:09 +0530

    show generated interviews on the home page

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -11,7 +11,7 @@ const Page = async () => {
       <Agent
         userName={user?.name!}
         userId={user?.id}
-        profileImage={user?.profileURL}
+        // profileImage={user?.profileURL}
         type="generate"
       />
     </>
